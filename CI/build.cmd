051fa0f8074f3431bd4dc3f7f838b95278b685f4
Zachary Lund <zachary.lund@streamlabs.com>
2018-03-12 12:16:42 -0700

    Adapt script for new browser plugin changes

--- CI/build.cmd
+++ CI/build.cmd
@@ -1,5 +1,15 @@
 cmake ^
 	-G"%CmakeGenerator%" ^
+	-A x64 ^
+	-T "host=x64" ^
+	-H"%CefPath%" ^
+	-B"%CefBuildPath%" ^
+	-DCEF_RUNTIME_LIBRARY_FLAG="/Md"
+
+cmake --build "%CefBuildPath%" --config "%BuildConfig%"
+
+cmake ^
+	-G"%CmakeGenerator%" ^
 	-T "host=x64" ^
 	-H"%APPVEYOR_BUILD_FOLDER%" ^
 	-B"%BuildPath32%" ^
@@ -21,8 +31,10 @@ cmake ^
 	-DCOPIED_DEPENDENCIES=false ^
 	-DCOPY_DEPENDENCIES=true ^
 	-DENABLE_SCRIPTING=false ^
+	-DCEF_ROOT_DIR=%CefPath% ^
+	-DBUILD_BROWSER=true ^
 	-DCOMPILE_D3D12_HOOK=true
-	
+
 cmake --build "%BuildPath32%\plugins\win-capture\get-graphics-offsets" --config "%BuildConfig%" --target install
 cmake --build "%BuildPath32%\plugins\win-capture\graphics-hook" --config "%BuildConfig%" --target install
 cmake --build "%BuildPath32%\plugins\win-capture\inject-helper" --config "%BuildConfig%" --target install
